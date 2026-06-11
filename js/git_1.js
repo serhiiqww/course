f792eec49dd64c2fb0a417bbeaed8a98790aacec
serhiiqw <serhiiqw@gmail.com>
2026-06-11 06:19:09 -0400

    3rd commit

---
+++ js/git_1.js
@@ -0,0 +1,10 @@
+"use strict";
+
+//git init - inicialization of this repo
+//git status - check all commits
+//git add
+//git commit -a -m - all files and message
+//git checkout <file> - revert to th last commit
+//git log - all commits
+//git remote add origin - connect local with git repo
+//git push -u origin main - push
